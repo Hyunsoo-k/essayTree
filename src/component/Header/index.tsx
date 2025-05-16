1e7dfb7edfae8c00ba397a247ffc632af35cd37f
Hyunsoo_kim <red_panda119@naver.com>
2025-05-17 01:47:32 +0900

    Feat : 컴포넌트 분리를 확실하게 구분할 수 있도록 리펙토링, 모달을 여는 아이콘 클릭시 발생하는 마우스 이벤트와 모달의 외부영역을 마우스다운 했을 시 발생하는 중복 호출을 방지하기 위해 이벤트핸들러에 stopPropagation 추가

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,113 +1,59 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import { useEffect, useState, useRef } from "react";
-import useViewportObserver from "@/hook/useViewportObserver";
+import { useState, useRef, MouseEvent } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 import { RiAccountBoxLine } from "react-icons/ri";
 import { RxHamburgerMenu } from "react-icons/rx";
 
+import useViewportObserver from "@/hook/useViewportObserver";
 import useViewportStore from "@/store/useViewportStore";
+import useHeaderShadowOnScroll from "@/hook/useHeaderShadowOnScroll";
+import TopBar from "../HeaderParts/TopBar";
+import ProfileModal from "../Modal/ProfileModal";
+import HeaderMenuDropdown from "../HeaderParts/HeaderMenuDropdown";
 
 import styles from "./index.module.scss";
 
-const Header = ():JSX.Element => {
+const Header = (): JSX.Element => {
   useViewportObserver();
-
   const { viewport } = useViewportStore();
 
-  const [locatedTop, setLocatedTop] = useState<boolean>(true);
-  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState<boolean>(false);
+  const [isMenuDropdownOpen, setIsMenuDropdownOpen] = useState<boolean>(false);
 
-  const hamburgerWrapperRef = useRef<HTMLDivElement | null>(null);
-  const dropdownRef = useRef<HTMLUListElement | null>(null);
+  const headerComponentRef = useRef<HTMLElement | null>(null);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.pageYOffset === 0) {
-        setLocatedTop(true);
-      } else {
-        setLocatedTop(false);
-      };
-    };
+  useHeaderShadowOnScroll(headerComponentRef);
 
-    window.addEventListener("scroll", handleScroll);
+  /**
+   * 모달이 열려있을 시, 모달의 외부영역에서 mousedown 이벤트 발생 시 click 이벤트와의 중복 호출을 막기 위한 함수
+   */
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [])
-
-  useEffect(() => {
-    const handleClickOutside = (e: globalThis.MouseEvent) => {
-      const targetNode = e.target as Node;
-
-      if (hamburgerWrapperRef.current?.contains(targetNode)) {
-        return;
-      };
-
-      if (!dropdownRef.current?.contains(targetNode)) {
-        setIsDropdownOpen(false);
-      };
-    };
-
-    window.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      window.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  const handleClickHamburger = (): void => {
-    setIsDropdownOpen((prev) => !prev);
+  const handlePreventMousedownEvent = (e: MouseEvent<HTMLElement>): void => {
+    e.stopPropagation();
   };
 
-  return(
-    <header className={
-      `${styles["component-container"]} ${locatedTop ? styles["located-top"] : styles["located-no-top"]}`
-    }>
-      {viewport !== "mobile" && (
-        <>
-          <div className={styles["top"]}>
-          <ul>
-            <li>
-              <Link href="/">Contact us</Link>
-            </li>
-            <li>
-              <Link href="/">Help</Link>
-            </li>
-          </ul>
-          </div>
-          <div className={styles["boundary-line"]}></div>
-        </>
-      )}
+  const handleClickProfileIconWrapper = (e: MouseEvent<HTMLElement>): void => {
+    e.stopPropagation();
+
+    setIsProfileModalOpen((prev: boolean) => !prev);
+  };
+
+  const handleClickmenuIconWrapper = (e: MouseEvent<HTMLLIElement>): void => {
+    e.stopPropagation();
+
+    setIsMenuDropdownOpen((prev: boolean) => !prev);
+  };
+
+  return (
+    <header ref={headerComponentRef} className={styles["component-container"]}>
+      {viewport !== "mobile" && <TopBar />}
       <div className={styles["main"]}>
         <h1>Essay tree</h1>
-        {viewport === "mobile" && (
-          <div className={styles["interection-box--mobile"]}>
-            <IoSearchOutline
-              size={23}
-              color="#7c7c7c"
-            />
-            <RiAccountBoxLine
-              size={25}
-              color="#7c7c7c"
-            />
-            <div ref={hamburgerWrapperRef}>
-              <RxHamburgerMenu
-                size={25}
-                color="#7c7c7c"
-                onClick={handleClickHamburger}
-              />
-            </div>
-          </div>
-        )}
         {viewport !== "mobile" && (
-          <>
-            <ul className={styles["navigation-bar"]}>
-              <li>
-                <Link href="/">Home</Link>
-              </li>
+          <nav>
+            <ul>
               <li>
                 <Link href="/">Notice</Link>
               </li>
@@ -117,37 +63,56 @@ const Header = ():JSX.Element => {
               <li>
                 <Link href="/">Articles</Link>
               </li>
+              <li>
+                <Link href="/">Athors</Link>
+              </li>
             </ul>
-            <div className={styles["interection-box--tablet"]}>
-              <IoSearchOutline
-                size={25}
-                color="#7c7c7c"
-              />
-              <RiAccountBoxLine
-                size={25}
-                color="#7c7c7c"
-              />
+          </nav>
+        )}
+        <ul className={styles["icon-box"]}>
+          <li>
+            <IoSearchOutline
+              size={viewport === "mobile" ? 23 : 25}
+              color="#7c7c7c"
+            />
+          </li>
+          <li
+            onMouseDown={handlePreventMousedownEvent}
+            onClick={handleClickProfileIconWrapper}
+          >
+            <RiAccountBoxLine
+              size={viewport === "mobile" ? 23 : 25}
+              color="#7c7c7c"
+            />
+          </li>
+          {viewport === "mobile" ? (
+            <li
+              onMouseDown={handlePreventMousedownEvent}
+              onClick={handleClickmenuIconWrapper}
+            >
+              <RxHamburgerMenu size={25} color="#7c7c7c" />
+            </li>
+          ) : (
+            <li>
               <button type="button">Post Essay</button>
-            </div>
-          </>
+            </li>
+          )}
+        </ul>
+        {isProfileModalOpen && (
+          <div className={styles["profile-modal-wrapper"]}>
+            <ProfileModal
+              isProfileModalOpen={isProfileModalOpen}
+              setIsProfileModalOpen={setIsProfileModalOpen}
+            />
+          </div>
         )}
       </div>
       {viewport === "mobile" && (
-          <ul className={`${styles["drop-down--mobile"]} ${isDropdownOpen ? styles["drop-down--open"] : styles["drop-down--close"]}`}>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/">Notice</Link>
-            </li>
-            <li>
-              <Link href="/">Guide</Link>
-            </li>
-            <li>
-              <Link href="/">Articles</Link>
-            </li>
-          </ul>
-        )}
+        <HeaderMenuDropdown
+          isMenuDropdownOpen={isMenuDropdownOpen}
+          setIsMenuDropdownOpen={setIsMenuDropdownOpen}
+        />
+      )}
     </header>
   );
 };
