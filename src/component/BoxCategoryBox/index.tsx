098bc0dfd256f7f46aee323525ee18163f5922e0
Hyunsoo_kim <red_panda119@naver.com>
2025-05-28 15:36:40 +0900

    Feat : BoxCateogry에서 사용하는 변수를 전역변수로 변경 후 import해서 사용

--- src/component/BoxCategoryBox/index.tsx
+++ src/component/BoxCategoryBox/index.tsx
@@ -1,25 +1,9 @@
-import BoxCategory from "../BoxCategory";
+import categoryArray from "@/variable/categoryArray";
+import BoxCategory from "@/component/BoxCategory";
+
 import styles from "./index.module.scss";
 
 const BoxCategoryBox = (): JSX.Element => {
-  const categoryArray = [
-    "일상",
-    "여행",
-    "음식",
-    "건강·운동",
-    "도서",
-    "사진",
-    "영화",
-    "동물",
-    "식물",
-    "음악",
-    "역사",
-    "경제",
-    "IT",
-    "미술",
-    "연애"
-  ];
-  
   return (
     <ul className={styles["component-container"]}>
       {categoryArray.map((category: string, index: number) => (
